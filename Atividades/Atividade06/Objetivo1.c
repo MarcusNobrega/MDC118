2c196461ca29b7aaa5e92ac7137b0d9f52ddd467
MarcusNobrega <69904672+MarcusNobrega@users.noreply.github.com>
2020-12-01 17:01:46 -0300

    Create Objetivo1.c

---
+++ Atividades/Atividade06/Objetivo1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct data {
+    int dia;
+    int mes;
+    int ano;
+} defData;
+
+int main(){
+    struct data defData;
+
+    printf("Coloque o dia: ");
+    scanf("%d", &defData.dia);
+
+    printf("Coloque o número do mês: ");
+    scanf("%d", &defData.mes);
+
+    printf("Coloque o ano: ");
+    scanf("%d", &defData.ano);
+
+    printf("Padrão brasileiro: ");
+    printf("%d\\%d\\%d \n", defData.dia, defData.mes, defData.ano);
+
+    printf("Padrão americano: ");
+    printf("%d\\%d\\%d \n", defData.mes, defData.dia, defData.ano);
+
+}
